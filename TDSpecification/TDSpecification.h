a66e4bc7267aab3b25dd79111d4c191be5f99085
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2016-05-26 21:19:19 -0500

    * initial add

---
+++ TDSpecification/TDSpecification.h
@@ -0,0 +1,19 @@
+//
+//  TDSpecification.h
+//  TDSpecification
+//
+//  Created by Todd Ditchendorf on 5/26/16.
+//  Copyright © 2016 Celestial Teapot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TDSpecification.
+FOUNDATION_EXPORT double TDSpecificationVersionNumber;
+
+//! Project version string for TDSpecification.
+FOUNDATION_EXPORT const unsigned char TDSpecificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDSpecification/PublicHeader.h>
+
+
